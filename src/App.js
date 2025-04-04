0ca44b232a6c1387bba6c2c6a82d652ecaa9ae35
hyunwoo <kshsim98@naver.com>
2025-04-04 22:58:51 +0900

    다시 수정

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ function App() {
     <Router>
       <Routes>
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
-        <Route path="/hello" element={<h1>hello</h1>}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route
+          path={process.env.PUBLIC_URL + "/hello"}
+          element={<h1>hello</h1>}
+        ></Route>
+        <Route
+          path={process.env.PUBLIC_URL + "/movie/:id"}
+          element={<Detail />}
+        ></Route>
       </Routes>
     </Router>
   );
