af5ce5b76ff18b6499203c1cde5f3904a2fec3a6
hyunwoo <kshsim98@naver.com>
2025-04-05 00:02:30 +0900

    css 복사

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loding, setLoading] = useState(true);
@@ -20,18 +21,22 @@ function Home() {
   }, []);
   console.log(movies);
   return (
-    <div>
+    <div className={styles.container}>
       {loding ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
               id={movie.id}
+              year={movie.year}
               coverImg={movie.medium_cover_image}
               title={movie.title}
               genres={movie.genres}
+              summary={movie.summary}
             />
           ))}
         </div>
